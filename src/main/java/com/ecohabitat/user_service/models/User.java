911cc9c0b543481c06cbec9532365d5be8d46587
peterParker79 <trypeterparker@gmail.com>
2025-05-22 19:18:10 +0200

    added user model

---
+++ src/main/java/com/ecohabitat/user_service/models/User.java
@@ -0,0 +1,23 @@
+package com.ecohabitat.user_service.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String email;
+    private long habitatId;
+}
