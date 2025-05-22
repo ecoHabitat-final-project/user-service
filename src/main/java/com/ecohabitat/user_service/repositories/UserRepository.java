b1826245d866a47d1d9fee927ebe7c4475cae617
peterParker79 <trypeterparker@gmail.com>
2025-05-22 19:29:31 +0200

    add user repository

---
+++ src/main/java/com/ecohabitat/user_service/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.ecohabitat.user_service.repositories;
+
+import com.ecohabitat.user_service.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
