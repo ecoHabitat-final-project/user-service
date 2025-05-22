e2b7fe802d7a9c36997681cc550bbcddef8a0c0a
peterParker79 <trypeterparker@gmail.com>
2025-05-22 20:39:33 +0200

    add CRUD and DTO and more tests

--- src/main/java/com/ecohabitat/user_service/services/UserService.java
+++ src/main/java/com/ecohabitat/user_service/services/UserService.java
@@ -1,11 +1,14 @@
 package com.ecohabitat.user_service.services;
 
+import com.ecohabitat.user_service.dto.EmailDTO;
 import com.ecohabitat.user_service.exceptions.UserNotFoundException;
+import com.ecohabitat.user_service.exceptions.UsersNotFoundException;
 import com.ecohabitat.user_service.models.User;
 import com.ecohabitat.user_service.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -14,7 +17,7 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    User getUserById(long id) {
+    public User getUserById(long id) {
         Optional<User> userOptional= userRepository.findById(id);
         if(userOptional.isPresent()) {
             return userOptional.get();
@@ -22,4 +25,40 @@ public class UserService {
             throw new UserNotFoundException("User id " + id + " not found");
         }
     }
+
+
+    public List<User> getUsers() {
+        if(userRepository.findAll().isEmpty()) {
+            throw new UsersNotFoundException("User are empty");
+
+        }else{
+            return userRepository.findAll();
+        }
+    }
+
+    public User getUserByEmail(String email) {
+
+        Optional<User> userOptional= userRepository.findUserByEmail(email);
+        if(userOptional.isPresent()) {
+            return userOptional.get();
+        }else{
+            throw new UserNotFoundException("User mail " + email + " not found");
+        }
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(User user) {
+        userRepository.delete(user);
+    }
+
+    public User updateMail(Long userId, EmailDTO emailDTO) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException("User id " + userId + " not found"));
+        user.setEmail(emailDTO.getEmail());
+        return userRepository.save(user);
+    }
+
 }
--- src/test/java/com/ecohabitat/user_service/services/UserServiceTest.java
+++ src/test/java/com/ecohabitat/user_service/services/UserServiceTest.java
@@ -2,15 +2,29 @@ package com.ecohabitat.user_service.services;
 
 import com.ecohabitat.user_service.models.User;
 import com.ecohabitat.user_service.repositories.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class UserServiceTest {
+    private User testUser;
+    @BeforeEach
+    public void setUp() {
+        testUser = new User();
+        testUser.setName("test");
+        testUser.setEmail("test@example.com");
+        userRepository.save(testUser);
+    }
+
+
+
     @Autowired
     private UserService userService;
 
@@ -22,9 +36,20 @@ class UserServiceTest {
     void testGetUserById() {
         User user = userService.getUserById(1);
         assertNotNull(user);
+    }
 
+    @Test
+    @DisplayName("Test get all users")
+    void testGetUsers() {
+        List<User> users = userService.getUsers();
+        assertNotNull(users);
+    }
 
-
+    @Test
+    @DisplayName("Test get user by email")
+    void testEmail() {
+        User user = userService.getUserByEmail("test@example.com");
+        assertEquals("test@example.com", user.getEmail());
     }
 
 
